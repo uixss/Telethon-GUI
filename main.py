c714f5a46db1ff24cc4bae8449a6efc0e1797ab7
uixss <uixsscve@gmail.com>
2024-10-30 10:20:04 -0300

    main.py

--- main.py
+++ main.py
@@ -2,21 +2,26 @@ import asyncio
 import os
 import pickle
 import customtkinter as ctk
-from tkinter import ttk, Toplevel, messagebox
+from tkinter import ttk, Toplevel
 from PIL import Image, ImageTk
 import ctypes  
-from logica import create_session, save_session, load_sessions, save_all_sessions, get_entity_data
 from telethon import TelegramClient, functions, types, errors
+from logica import create_session, save_session, load_sessions, save_all_sessions, get_entity_data
+import base64
+import io
+
 try:
     ctypes.windll.shcore.SetProcessDpiAwareness(1)
 except Exception:
     pass
+
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
+
 root = ctk.CTk()
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
-window_width = int(screen_width * 0.3)
+window_width = int(screen_width * 0.25)
 window_height = int(screen_height * 0.48)
 root.geometry(f"{window_width}x{window_height}")
 root.title("𝐒𝐇𝐈𝐓 𝐔𝐍𝐃𝐄𝐑 💀")
@@ -26,17 +31,40 @@ root.overrideredirect(True)
 root.wm_attributes('-alpha', 0.92)
 root.grid_columnconfigure(0, weight=3) 
 root.grid_columnconfigure(1, weight=1)  
+icon_path = os.path.abspath("undermain.ico")
 
-
-def create_styled_popup(title, width=300, height=200):
+def create_generic_popup(title, width=250, height=400, labels=[], button_text="Aceptar", button_command=None):
     popup = ctk.CTkToplevel(root)
     popup.title(title)
-    popup.geometry(f'{width}x{height}')
-    popup.configure(bg='#262626')
-    popup.overrideredirect(True)
-    popup.wm_attributes('-alpha', 0.92)
+    if os.path.exists(icon_path):
+        popup.iconbitmap(icon_path)
+    else:
+        print("El icono undermain.ico no se encontró.")
+
+    popup.geometry(f"{width}x{height}")
+    popup.configure(bg="#262626")
     popup.resizable(False, False)
+    popup.wm_attributes('-alpha', 0.95)
+
+    input_entries = {}
+    for label_text in labels:
+        label = ctk.CTkLabel(popup, text=label_text, text_color="lightgray")
+        label.pack(pady=10)
+        entry = ctk.CTkEntry(popup)
+        entry.pack(pady=5)
+        input_entries[label_text] = entry
+
+    if button_command:
+        button = ctk.CTkButton(popup, text=button_text, command=lambda: button_command(input_entries),
+                               fg_color="#3A3A4D", hover_color="#6C6C8A")
+        button.pack(pady=10)
+    else:
+        button = ctk.CTkButton(popup, text=button_text, command=popup.destroy,
+                               fg_color="#3A3A4D", hover_color="#6C6C8A")
+        button.pack(pady=10)
+
     return popup
+
 
 def start_move(event):
     root.x = event.x
@@ -57,19 +85,6 @@ root.bind("<ButtonPress-1>", start_move)
 root.bind("<ButtonRelease-1>", stop_move)
 root.bind("<B1-Motion>", on_motion)
 
-def style_popup(popup):
-    popup.title("𝐒𝐇𝐈𝐓 𝐔𝐍𝐃𝐄𝐑 💀")
-    popup.geometry("400x400")
-    popup.configure(bg="#262626")
-    popup.resizable(False, False)
-    popup.wm_attributes('-alpha', 0.95)
-
-def open_popup(title, content):
-    popup = Toplevel(root)
-    style_popup(popup)
-    label = ctk.CTkLabel(popup, text=content, text_color="lightgray", bg_color="#262626", wraplength=380)
-    label.pack(padx=10, pady=10, expand=True)
-
 def update_sessions_list():
     active_sessions = load_sessions()
     for item in tree.get_children():
@@ -80,47 +95,17 @@ def update_sessions_list():
         tree.insert("", "end", values=(masked_phone,))
 
 
-def show_custom_warning(title, message):
-    popup = Toplevel(root)
-    style_popup(popup)
-    popup.title(title)
-    
-    label = ctk.CTkLabel(popup, text=message, text_color="lightgray", bg_color="#262626", wraplength=380)
-    label.pack(padx=20, pady=20)
-
-    close_button = ctk.CTkButton(popup, text="Aceptar", command=popup.destroy, fg_color="#3A3A4D", hover_color="#6C6C8A")
-    close_button.pack(pady=10)
-
-
 def delete_selected_session():
     selected_item = tree.selection()
     if not selected_item:
-        show_custom_warning("Advertencia", "Seleccione una sesión para eliminar.")
+        create_generic_popup("Advertencia", labels=["Seleccione una sesión para eliminar."])
         return
     item = tree.item(selected_item)
     session_data = item['values']
     updated_sessions = [s for s in load_sessions() if f"{s[2][:3]}{'*' * (len(s[2]) - 3)}" != session_data[0]]
     save_all_sessions(updated_sessions)
     update_sessions_list()
-    show_custom_warning("Eliminado", "La sesión ha sido eliminada y los cambios se han guardado.")
-
-
-def show_custom_message(title, message):
-    popup = Toplevel(root)
-    style_popup(popup)
-    popup.title(title)
-
-
-    icon_label = ctk.CTkLabel(popup, text="⚠️", text_color="yellow", font=("Arial", 32), bg_color="#262626")
-    icon_label.pack(pady=(20, 10))
-
-
-    message_label = ctk.CTkLabel(popup, text=message, text_color="lightgray", bg_color="#262626", wraplength=380)
-    message_label.pack(padx=20, pady=10)
-
-
-    close_button = ctk.CTkButton(popup, text="Aceptar", command=popup.destroy, fg_color="#3A3A4D", hover_color="#6C6C8A")
-    close_button.pack(pady=20)
+    create_generic_popup("Eliminado", labels=["La sesión ha sido eliminada y los cambios se han guardado."])
 
 
 def authenticate():
@@ -131,7 +116,8 @@ def authenticate():
         asyncio.run(create_session(api_id, api_hash, phone_number))
         update_sessions_list()
     else:
-        show_custom_message("Advertencia", "Por favor complete todos los campos antes de autenticar.")
+        create_generic_popup("Advertencia", labels=["Por favor complete todos los campos antes de autenticar."])
+
 
 async def check_entity():
     entity_input = entity_input_entry.get()
@@ -144,34 +130,31 @@ async def check_entity():
             entity = await client.get_entity(entity_input)
             if isinstance(entity, types.Channel) or isinstance(entity, types.Chat):
                 full_chat = await client(functions.channels.GetFullChannelRequest(channel=entity))
-                banned = getattr(full_chat.full_chat, 'banned_rights', None) is not None
-                restricted = getattr(full_chat.full_chat, 'restrictions', None) is not None
-                ban_status = "permanently banned" if banned or restricted else "active"
+                ban_status = "banned" if getattr(full_chat.full_chat, 'banned_rights', None) else "active"
                 
                 console_text.delete("1.0", ctk.END)
-                console_text.insert(ctk.END, f"❌ {entity_input} has just been {ban_status}.\n"
+                console_text.insert(ctk.END, f"❌ {entity_input} has been {ban_status}.\n"
                                          f"\n• Chat ID: {entity.id}\n"
-                                         f"• Chat Name {full_chat.chats[0].title}\n"
-                                         f"• Chat Members {full_chat.full_chat.participants_count}\n")
+                                         f"• Chat Name: {full_chat.chats[0].title}\n"
+                                         f"• Chat Members: {full_chat.full_chat.participants_count}\n")
             elif isinstance(entity, types.User):
                 user_info = await client(functions.users.GetFullUserRequest(entity))
                 user = user_info.users[0]
                 
                 console_text.delete("1.0", ctk.END)
                 console_text.insert(ctk.END, f"❌ {entity_input} \n"
-                                                         f"\n\• ID {user.id}\n"
-                                         f"n• Name {user.first_name} {user.last_name or ''}\n"
-
-                                         f"• Username @{user.username or 'N/A'}\n")
+                                         f"\n• ID: {user.id}\n"
+                                         f"• Name: {user.first_name} {user.last_name or ''}\n"
+                                         f"• Username: @{user.username or 'N/A'}\n")
         except errors.UserNotParticipantError:
             console_text.delete("1.0", ctk.END)
-            console_text.insert(ctk.END, f"El usuario {phone} no es participante del canal o grupo: {entity_input}.\n")
+            console_text.insert(ctk.END, f"User {phone} is not a participant in {entity_input}.\n")
         except Exception as e:
             console_text.delete("1.0", ctk.END)
-            console_text.insert(ctk.END, f"Error al obtener entidad: {str(e)}\n")
+            console_text.insert(ctk.END, f"Error retrieving entity: {str(e)}\n")
     else:
         console_text.delete("1.0", ctk.END)
-        console_text.insert(ctk.END, f"No autorizado para la sesión inicial: {phone}\n")
+        console_text.insert(ctk.END, f"Not authorized for initial session: {phone}\n")
     await client.disconnect()
 
 
@@ -179,21 +162,6 @@ def run_check_entity():
     console_text.delete("1.0", "end") 
     asyncio.run(check_entity())
 
-
-def style_popup(popup):
-    popup.title("𝐒𝐇𝐈𝐓 𝐔𝐍𝐃𝐄𝐑 💀")
-    popup.geometry("400x400")
-    popup.configure(bg="#262626")
-    popup.resizable(False, False)
-    popup.wm_attributes('-alpha', 0.95)
-
-def open_popup(title, content):
-    popup = Toplevel(root)
-    style_popup(popup)
-    popup.title(title)
-    
-    label = ctk.CTkLabel(popup, text=content, text_color="lightgray", bg_color="#262626", wraplength=380)
-    label.pack(padx=10, pady=10, expand=True)
 
 top_panel = ctk.CTkFrame(root, height=40, fg_color="#121212")
 top_panel.grid(row=0, column=0, columnspan=2, padx=10, pady=(10, 0), sticky="ew")
@@ -206,17 +174,16 @@ main_frame.grid_columnconfigure(1, weight=1)
 index_frame = ctk.CTkFrame(root, corner_radius=5, fg_color="#262626")
 index_frame.grid(row=1, column=1, padx=10, pady=10, sticky="nsew")
 
-
 buttons = [
     ("About", "Luxury Shield version 1.0\nDeveloped by [Your Company]."),
     ("Support", "For support, please contact support@example.com."),
-    ("Update", "Check for updates to get the latest features.")
+   
 ]
 
 for i, (text, content) in enumerate(buttons):
     button = ctk.CTkButton(
         top_panel, text=text, fg_color="#121212", hover_color="#3A3A4D",
-        width=100, command=lambda t=text, c=content: open_popup(t, c)
+        width=225, command=lambda t=text, c=content: create_generic_popup(t, labels=[content])
     )
     button.grid(row=0, column=i, padx=5, pady=5)
 
@@ -229,7 +196,6 @@ for i, label_text in enumerate(labels):
     entry.grid(row=i, column=1, padx=5, pady=5, sticky="ew")
     entries.append(entry)
 api_id_entry, api_hash_entry, phone_entry = entries
-
 
 authenticate_button = ctk.CTkButton(main_frame, text="AUTH", command=authenticate, fg_color="#3A3A4D", hover_color="#6C6C8A")
 authenticate_button.grid(row=3, column=1, pady=10, sticky="ew")
@@ -268,12 +234,14 @@ if os.path.exists(logo_path):
     canvas.pack(pady=(10, 10))
 
 buttons = [
-    ("Send Messag", lambda: print("Send Message User clicked")),
-    ("Get INFO", lambda: print("Get ADMINS clicked")),
-
+    ("Send Message", lambda: create_generic_popup("Send Message", labels=["Username or ID", "Message", "Number of Sessions", "Messages per Session"], button_text="Send Message", button_command=lambda entries: asyncio.run(send_messages_popup(entries)))),
+    ("Get INFO", lambda: print("Get INFO clicked")),
+    ("MASSACRE REPORT", lambda: print("MASSACRE REPORT clicked"))
 ]
+
 for text, command in buttons:
     button = ctk.CTkButton(index_frame, text=text, command=command, fg_color="#3A3A4D", hover_color="#6C6C8A", height=25)
     button.pack(pady=5, padx=5, fill="x")
+
 
 root.mainloop()
